4fa133bcbaf12cf114c9f2cf44eab03a591ea2ea
David Rhoden <david@davidrhoden.com>
2026-06-05 03:26:23 -0500

    configuration

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "MCK Millinery",
+	url: "https://mckmillinery.com/",
+	language: "en",
+	description: "The hats of MCK Millinery.",
+	author: {
+		name: "Melanie Claire",
+		email: "melanie@mckmillinery.com",
+		url: "https://mckmillinery.com/about-mck/"
+	}
+}
